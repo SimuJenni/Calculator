52a0b913b661899129371f5c29f7b038ddddbb80
Simon Jenni <simujenni@students.unibe.ch>
2015-07-07 00:47:49 +0200

    Added sin, cos and pi to the calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,7 +36,6 @@ class ViewController: UIViewController {
         }
     }
     
-    
     var operandStack = Array<Double>()
     
     @IBAction func operation(sender: UIButton) {
@@ -50,6 +49,10 @@ class ViewController: UIViewController {
         case "+": performOperation {$1 + $0}
         case "−": performOperation {$1 - $0}
         case "√": performOperation {sqrt($0)}
+        case "sin": performOperation {sin($0)}
+        case "cos": performOperation {cos($0)}
+        case "π": display.text = M_PI.description
+            enter()
         default: break
         }
     }
@@ -72,6 +75,7 @@ class ViewController: UIViewController {
         userIsTyping = false
         operandStack.append(displayValue)
         println("\(operandStack)")
+        isFloat = false
     }
     
     var displayValue: Double {
